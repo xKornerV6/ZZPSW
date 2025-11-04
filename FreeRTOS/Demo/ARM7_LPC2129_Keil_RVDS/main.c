ebadbca9886dd696d1d1a62e671978f39e193883
xKornerV6 <kluczbas@gmail.com>
2025-11-04 17:19:17 +0100

    2.2.1

--- FreeRTOS/Demo/ARM7_LPC2129_Keil_RVDS/main.c
+++ FreeRTOS/Demo/ARM7_LPC2129_Keil_RVDS/main.c
@@ -12,13 +12,21 @@ void Delay(unsigned int uiMiliSec) {
 void Led0Blink(void *pvParameters) {
     while (1) {
         Led_Toggle(0);
-        Delay(1000);
+        Delay(1000);   // 0.5 Hz
+    }
+}
+
+void Led1Blink(void *pvParameters) {
+    while (1) {
+        Led_Toggle(1);
+        Delay(50);     // 10 Hz (pelny cykl = 100 ms)
     }
 }
 
 int main(void) {
     Led_Init();
     xTaskCreate(Led0Blink, NULL, 100, NULL, 2, NULL);
+    xTaskCreate(Led1Blink, NULL, 100, NULL, 2, NULL);
     vTaskStartScheduler();
     while (1);
 }
