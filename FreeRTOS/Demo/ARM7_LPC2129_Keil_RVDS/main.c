475ed288b0db668a9617a4a6058a92750042365a
xKornerV6 <kluczbas@gmail.com>
2025-11-04 18:03:04 +0100

    3.1.3

--- FreeRTOS/Demo/ARM7_LPC2129_Keil_RVDS/main.c
+++ FreeRTOS/Demo/ARM7_LPC2129_Keil_RVDS/main.c
@@ -2,6 +2,11 @@
 #include "task.h"
 #include "led.h"
 
+typedef struct {
+    unsigned char ucLedNumber;
+    unsigned char ucFreq;
+} LedParams;
+
 void Delay(unsigned int uiMiliSec) {
     unsigned int uiLoopCtr, uiDelayLoopCount;
     uiDelayLoopCount = uiMiliSec * 12000;
@@ -10,29 +15,38 @@ void Delay(unsigned int uiMiliSec) {
 }
 
 void LedBlink(void *pvParameters) {
-    unsigned char *pucFreq = (unsigned char *)pvParameters;
+    LedParams *pLed = (LedParams *)pvParameters;
     while (1) {
-        Led_Toggle(0);
-        vTaskDelay(pdMS_TO_TICKS((1000 / *pucFreq) / 2));
+        Led_Toggle(pLed->ucLedNumber);
+        vTaskDelay(pdMS_TO_TICKS((1000 / pLed->ucFreq) / 2));
     }
 }
 
 void LedCtrl(void *pvParameters) {
-    unsigned char *pucFreq = (unsigned char *)pvParameters;
+    LedParams *pLed = (LedParams *)pvParameters;
+    unsigned char ucCounter = 0;
     while (1) {
-        (*pucFreq)++;
-        if (*pucFreq > 10) {
-            *pucFreq = 1;
+        pLed->ucFreq++;
+        if (pLed->ucFreq > 10) {
+            pLed->ucFreq = 1;
+        }
+        ucCounter++;
+        if (ucCounter >= 2) {
+            ucCounter = 0;
+            pLed->ucLedNumber++;
+            if (pLed->ucLedNumber > 1) {
+                pLed->ucLedNumber = 0;
+            }
         }
         vTaskDelay(pdMS_TO_TICKS(1000));
     }
 }
 
 int main(void) {
-    static unsigned char ucBlinkingFreq = 1;
+    static LedParams xLedParams = {0, 1};
     Led_Init();
-    xTaskCreate(LedBlink, NULL, 100, &ucBlinkingFreq, 2, NULL);
-    xTaskCreate(LedCtrl, NULL, 100, &ucBlinkingFreq, 2, NULL);
+    xTaskCreate(LedBlink, NULL, 100, &xLedParams, 2, NULL);
+    xTaskCreate(LedCtrl, NULL, 100, &xLedParams, 2, NULL);
     vTaskStartScheduler();
     while (1);
 }
