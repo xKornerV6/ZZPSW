f54db6ed4410f53cccda3c8dfe80bb62399fa0a4
xKornerV6 <kluczbas@gmail.com>
2025-11-04 18:15:16 +0100

    4.1.4

--- FreeRTOS/Demo/ARM7_LPC2129_Keil_RVDS/main.c
+++ FreeRTOS/Demo/ARM7_LPC2129_Keil_RVDS/main.c
@@ -12,32 +12,27 @@ void PulseTrigger(void *pvParameters) {
     }
 }
 
-void Pulse_LED0(void *pvParameters) {
+void Pulse_LED(void *pvParameters) {
+    unsigned char ucLedNum = *((unsigned char*)pvParameters);
     while (1) {
         if (xSemaphoreTake(xPulseSemaphore, portMAX_DELAY) == pdTRUE) {
-            Led_Toggle(0);
+            Led_Toggle(ucLedNum);
             vTaskDelay(pdMS_TO_TICKS(100));
-            Led_Toggle(0);
-        }
-    }
-}
-
-void Pulse_LED1(void *pvParameters) {
-    while (1) {
-        if (xSemaphoreTake(xPulseSemaphore, portMAX_DELAY) == pdTRUE) {
-            Led_Toggle(1);
-            vTaskDelay(pdMS_TO_TICKS(100));
-            Led_Toggle(1);
+            Led_Toggle(ucLedNum);
         }
     }
 }
 
 int main(void) {
+    static unsigned char ucLed0 = 0;
+    static unsigned char ucLed1 = 1;
+
     Led_Init();
     xPulseSemaphore = xSemaphoreCreateBinary();
     xTaskCreate(PulseTrigger, NULL, 100, NULL, 2, NULL);
-    xTaskCreate(Pulse_LED0, NULL, 100, NULL, 2, NULL);
-    xTaskCreate(Pulse_LED1, NULL, 100, NULL, 2, NULL);
+    xTaskCreate(Pulse_LED, NULL, 100, &ucLed0, 2, NULL);
+    xTaskCreate(Pulse_LED, NULL, 100, &ucLed1, 2, NULL);
     vTaskStartScheduler();
+
     while (1);
 }
