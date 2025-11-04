354da8062297444a2679581e6d67fe97da012be2
xKornerV6 <kluczbas@gmail.com>
2025-11-04 17:21:10 +0100

    2.2.2

--- FreeRTOS/Demo/ARM7_LPC2129_Keil_RVDS/main.c
+++ FreeRTOS/Demo/ARM7_LPC2129_Keil_RVDS/main.c
@@ -2,24 +2,17 @@
 #include "task.h"
 #include "led.h"
 
-void Delay(unsigned int uiMiliSec) {
-    unsigned int uiLoopCtr, uiDelayLoopCount;
-    uiDelayLoopCount = uiMiliSec * 12000;
-    for (uiLoopCtr = 0; uiLoopCtr < uiDelayLoopCount; uiLoopCtr++) {
-    }
-}
-
 void Led0Blink(void *pvParameters) {
     while (1) {
         Led_Toggle(0);
-        Delay(1000);   // 0.5 Hz
+        vTaskDelay(pdMS_TO_TICKS(1000));   // 0.5 Hz
     }
 }
 
 void Led1Blink(void *pvParameters) {
     while (1) {
         Led_Toggle(1);
-        Delay(50);     // 10 Hz (pelny cykl = 100 ms)
+        vTaskDelay(pdMS_TO_TICKS(50));     // 10 Hz
     }
 }
 
