a5bb91c271a938229d77954c4d3d886a8bd5b3d2
xKornerV6 <kluczbas@gmail.com>
2025-11-04 17:16:51 +0100

    2.1.2

--- FreeRTOS/Demo/ARM7_LPC2129_Keil_RVDS/bin/main.d
+++ FreeRTOS/Demo/ARM7_LPC2129_Keil_RVDS/bin/main.d
@@ -1,3 +1,14 @@
 .\bin\main.o: main.c
+.\bin\main.o: ..\..\Source\include\FreeRTOS.h
+.\bin\main.o: C:\Keil\ARM\ARMCC\bin\..\include\stddef.h
+.\bin\main.o: C:\Keil\ARM\ARMCC\bin\..\include\stdint.h
+.\bin\main.o: .\FreeRTOSConfig.h
 .\bin\main.o: C:\Keil\ARM\Inc\Philips\lpc21xx.h
+.\bin\main.o: ..\..\Source\include\projdefs.h
+.\bin\main.o: ..\..\Source\include\portable.h
+.\bin\main.o: ..\..\Source\include\deprecated_definitions.h
+.\bin\main.o: ..\..\Source\include\..\..\Source\portable\RVDS\ARM7_LPC21xx\portmacro.h
+.\bin\main.o: ..\..\Source\include\mpu_wrappers.h
+.\bin\main.o: ..\..\Source\include\task.h
+.\bin\main.o: ..\..\Source\include\list.h
 .\bin\main.o: led.h
--- FreeRTOS/Demo/ARM7_LPC2129_Keil_RVDS/main.c
+++ FreeRTOS/Demo/ARM7_LPC2129_Keil_RVDS/main.c
@@ -1,26 +1,24 @@
-#include <lpc21xx.h>
+#include "FreeRTOS.h"
+#include "task.h"
 #include "led.h"
 
-void Delay(unsigned int uiMiliSec)
-{
+void Delay(unsigned int uiMiliSec) {
     unsigned int uiLoopCtr, uiDelayLoopCount;
-    uiDelayLoopCount = uiMiliSec*12000;
-    for(uiLoopCtr=0;uiLoopCtr<uiDelayLoopCount;uiLoopCtr++) {}
-}
-int main( void )
-{
-    Led_Init();
-    while(1)
-    {
-        Led_Toggle(0);
-        Led_Toggle(1);
-        Delay(125);
-        Led_Toggle(1);
-        Delay(125);
-        Led_Toggle(1);
-        Delay(125);
-        Led_Toggle(1);
-        Delay(125);
+    uiDelayLoopCount = uiMiliSec * 12000;
+    for (uiLoopCtr = 0; uiLoopCtr < uiDelayLoopCount; uiLoopCtr++) {
     }
+}
 
+void Led0Blink(void *pvParameters) {
+    while (1) {
+        Led_Toggle(0);
+        Delay(1000);
+    }
+}
+
+int main(void) {
+    Led_Init();
+    xTaskCreate(Led0Blink, NULL, 100, NULL, 2, NULL);
+    vTaskStartScheduler();
+    while (1);
 }
